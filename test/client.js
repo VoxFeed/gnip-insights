123a319d7d5c6842e946043cf6154b56cb2b8ea5
Gus Ortiz <guspidey@gmail.com>
2016-05-20 13:01:56 -0500

    restructures, removes get method and adds tests

---
+++ test/client.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const test = require('tape');
+const nock = require('nock');
+
+const gnip = require('./../lib/client');
+const gnipResponse = require('./responses/gnip-success-response');
+
+const API_URL = 'https://data-api.twitter.com';
+const TOTALS_PATH = 'insights/engagement/totals';
+
+const credentials = {
+  consumerKey: 'consumerKey',
+  consumerSecret: 'consumerSecret',
+  accessToken: 'accessToken',
+  accessTokenSecret: 'accessTokenSecret'
+};
+
+test('it should throw bad credentials error', (assert) => {
+  let error;
+  try {
+    gnip({});
+  } catch (err) {
+    error = err;
+  }
+
+  assert.equal(error.message, 'invalid credentials');
+  assert.end();
+});
+
+test('it should return object when json response', (assert) => {
+  nock(API_URL).post('/' + TOTALS_PATH).reply(200, gnipResponse);
+
+  const request = gnip(credentials);
+
+  request(TOTALS_PATH, {}, (error, data) => {
+    if (error) return assert.end(error);
+    assert.equal(data.errors.length, 1);
+    assert.equal(data.totals['423456789'].favorites, '67');
+
+    nock.cleanAll();
+    assert.end();
+  });
+});
+
+test('it should return error when request fails', (assert) => {
+  nock(API_URL).post('/' + TOTALS_PATH).reply(500);
+
+  const request = gnip(credentials);
+
+  request(TOTALS_PATH, {}, (error, data) => {
+    if (data) return assert.end(new Error('unexpected data'));
+    assert.equal(!!error, true);
+
+    nock.cleanAll();
+    assert.end();
+  });
+});
